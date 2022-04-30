b2716127051f1ae3cd17cb5384ce23140155a3b9
rabukakeher <bhattrahul712@gmail.com>
2022-04-30 12:50:06 -0400

    all tweet serarch initial commit

---
+++ TwitterLoginApi.py
@@ -0,0 +1,20 @@
+import twitter
+
+def get_twitter_api():
+    # XXX: Go to http://twitter.com/apps/new to create an app and get values
+    # for these credentials that you'll need to provide in place of these
+    # empty string values that are defined as placeholders.
+    # See https://developer.twitter.com/en/docs/basics/authentication/overview/oauth
+    # for more information on Twitter's OAuth implementation.
+    
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    OAUTH_TOKEN = ''
+    OAUTH_TOKEN_SECRET = ''
+    
+    auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
+                               CONSUMER_KEY, CONSUMER_SECRET)
+    
+    twitter_api = twitter.Twitter(auth=auth)
+    return twitter_api
+ 
